02ded335f28094b734397509508b0090254d0dc3
awaludin16 <awal72304@gmail.com>
2024-05-02 21:20:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Imron Bakes</title>
+</head>
+<body>
+    
+    <!-- header section start -->
+    <header>
+        <a href="#" class="logo">Imron<span>bakes.</span></a>
+        <nav class="navbar">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#products">Products</a>
+            <a href="#contact">Contact</a>
+        </nav>
+        <div class="icon">
+            <a href="#" class="trolley-cart"><img src="icon/trolley-cart.png"></a>
+            <a href="#" id="hamburger-menu"><img src="icon/hamburger-menu.png"></a>
+        </div>
+    </header>
+    <!-- header section end -->
+
+    <!-- Hero section start -->
+    <section class="hero" id="home">
+        <div class="content">
+            <h1>EID MUBARAK <span>HAMPERS</span></h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, optio?=mwehehehe</P>
+            <a href="#" class="cta">ORDER NOW</a>
+        </div>
+    </section>
+    <!-- Hero section end -->
+
+    <!-- About section start -->
+    <section id="about" class="about">
+        <h2>Tentang Kami</h2>
+        <div class="row">
+            <div class="about-img">
+                <img src="img/tentang-kami.jpg" alt="Tentang Kami">
+            </div>
+            <div class="content">
+                <h3>Jangan Beli Produk Kami</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptatem delectus iste pariatur quisquam eveniet!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dicta a in dolor, nisi quis asperiores excepturi officia alias sunt voluptate, dignissimos soluta, error perferendis.</p>
+            </div>
+        </div>
+    </section>
+    <!-- About section end -->
+
+
+    <!-- my javascript -->
+    <script src="js/script.js"></script>
+</body>
+</html>
